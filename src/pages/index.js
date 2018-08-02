48bcf2b45be201b90a812e3c43da8ad46ee8d910
Simon Richards <s.richards@dewynters.com>
2018-08-02 14:23:01 +0100

    new sentence

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <p>Deployed with Netlify... cool stuff</p>
+    <p>This is seriously good</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
