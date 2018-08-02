e0346b59e4ca4e4f9aee3cf6dd219cd187439e0c
Simon Richards <s.richards@dewynters.com>
2018-08-02 14:21:41 +0100

    push to master

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>Deployed with Netlify... cool stuff</p>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
